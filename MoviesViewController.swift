27de3c206f17ab11c3817c8ba5baf5a7b90c1ac7
Hannah Lily Postman <postman.hl@gmail.com>
2017-01-29 20:00:48 -0800

    Loading titles into cells from The Movie Database API of video 2 complete

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -11,12 +11,32 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var TableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         TableView.dataSource = self
         TableView.delegate = self
+
+        // Do any additional setup after loading the view.
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    
+                    self.movies = (dataDictionary["results"] as! [NSDictionary])
+                    self.TableView.reloadData()
+                }
+            }
+        }
+        task.resume()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +45,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-        
+        return movies?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
-        cell.textLabel?.text = "Here is a cell, woohoo! 😍 Its row is \(indexPath.row)."
         print(cell.textLabel?.text ?? "Something went wrong")
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as? String ?? "Error fetching title"
+        cell.textLabel?.text = title
         return cell
     }
 
