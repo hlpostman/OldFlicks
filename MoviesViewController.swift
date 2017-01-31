84accf84a490aa86ccccd61f033d1a4127092e32
Hannah Lily Postman <postman.hl@gmail.com>
2017-01-30 20:32:51 -0800

    Added progress Heads Up Display with MBProgressHUD cocoa pod.

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -20,6 +21,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        MBProgressHUD.showAdded(to: self.view, animated: true)
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
@@ -30,6 +32,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 }
             }
         }
+        print(task)
+        MBProgressHUD.hide(for: self.view, animated: true)
         return task
     }
     
